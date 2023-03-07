a510fa4fc17a81287a17de3756fb712a4e6445e4
“Maksim <m.n.timchenko@gmail.com>
2023-03-07 21:34:33 +1000

    Implement setScreenSize

--- src/main/java/timmax/games/minesweeper/MinesweeperGame.java
+++ src/main/java/timmax/games/minesweeper/MinesweeperGame.java
@@ -3,5 +3,10 @@ package timmax.games.minesweeper;
 import com.javarush.engine.cell.*;
 
 public class MinesweeperGame extends Game {
-
+    private static final int SIDE_OF_WIDTH = 30;
+    private static final int SIDE_OF_HEIGHT = 5;
+    @Override
+    public void initialize() {
+        setScreenSize(SIDE_OF_WIDTH, SIDE_OF_HEIGHT);
+    }
 }
